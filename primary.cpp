0ede741bc630e1d45d201738aa47ffad86a8ffa9
Pranjal Jain <pranjalworkon@gmail.com>
2023-09-24 19:41:17 +0530

    initial commmit

---
+++ primary.cpp
@@ -0,0 +1,84 @@
+// we use the c++ as it is more close to the hardware of the computer .
+// as it is a compiled language so it is more faster than the interpreterd language and more close to the hardware 
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"hello world\n";
+    cout<<"this is pranjal jain\n";
+    int a=5;
+    cout << a<<"\n";
+
+    // for writing the name of the variable and text separate the variable with the <<
+    cout<<"this is "<<a<<" a number\n";
+    // so in this way we can combine the variable and other string by using the <<
+
+    // for declaring a variable 
+
+    // taking input and showing output 
+    int age; //declaring the variable with its datatype 
+    cout<<"Enter the age";
+    cin>>age;
+    cout<<"your age is "<<age<<" years old\n";
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+
+
+}
+
+// COUT does not create the new line 
+//  use the \n for creating the new line 
+
+// C++ is more powerful than C as it has the concepts of the OOPS
+// in C++ we can do the memory management by ourdekf only 
+// the compiler converts the c++ program into the format that the computer can understand such as the low level lang or th emachine level lang which is th eexecutable format
+
+
+
+
+/*
+we can omit the use of the using namespace std by using the std keyword by ::
+
+#include <iostream>
+int main(){
+    std::cout<<"my name is pranjal jain";
+    return 0;
+}
+
+so remember that we can omit the use of the namespace by using the std::
+*/
+
+// char is denoted by the single quotes and string is denoted by the double quotes
+
+
+// variable declare 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
